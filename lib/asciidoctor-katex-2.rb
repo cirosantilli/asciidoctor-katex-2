a3621e1961b29cf07ccf62764d291817a1d08077
Ciro Santilli <ciro.santilli@gmail.com>
2019-08-13 00:00:01 +0000

    all but inline working with latexmath block identifier

--- lib/asciidoctor-katex-2.rb
+++ lib/asciidoctor-katex-2.rb
@@ -57,6 +57,15 @@ class KatexBlockMacroProcessor < Asciidoctor::Extensions::BlockMacroProcessor
   end
 end
 
+class KatexLatexmathBlockMacroProcessor < Asciidoctor::Extensions::BlockMacroProcessor
+  use_dsl
+  named :latexmath
+  def process parent, target, attrs
+    html = $katex.renderToString(attrs[1])
+    create_paragraph parent, html, attrs, subs: nil
+  end
+end
+
 class KatexInlineMacroProcessor < Asciidoctor::Extensions::InlineMacroProcessor
   use_dsl
   named :katex
@@ -82,9 +91,26 @@ class KatexDocinfoProcessor < Asciidoctor::Extensions::DocinfoProcessor
   end
 end
 
+class KatexTreeprocessor < Asciidoctor::Extensions::Treeprocessor
+  def process document
+    unless (stem_blocks = document.find_by context: :stem).nil_or_empty?
+      stem_blocks.each do |block|
+        if block.style == 'latexmath'
+          parent = block.parent
+          image = Asciidoctor::Block.new parent, :pass, source: '<div>' + $katex.renderToString(block.content) + '</div>'
+          parent.blocks[parent.blocks.index(block)] = image
+        end
+      end
+    end
+    document
+  end
+end
+
 Asciidoctor::Extensions.register do
   block KatexBlockProcessor
   block_macro KatexBlockMacroProcessor
+  block_macro KatexLatexmathBlockMacroProcessor
   docinfo_processor KatexDocinfoProcessor
   inline_macro KatexInlineMacroProcessor
+  treeprocessor KatexTreeprocessor
 end
